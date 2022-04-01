4a0ab83cbc2cd8ab67e34a7af91a552f54168281
NashDrilla <57951540+NashDrilla@users.noreply.github.com>
2022-04-01 16:10:28 +0100

    Renamed variable

--- GameFuncs.cpp
+++ GameFuncs.cpp
@@ -35,17 +35,17 @@ void GameFuncs::ApplyDrag(float BallisticCoeff, vec2& BulletVel, vec2& BulletPos
         velocityMult = (delta_speed / (1.0f - delta_speed)) + 1.0f;
     }
 
-    vec2 dragged{
+    vec2 dragged_vel{
         BulletVel.x * velocityMult,
         (this->gravity * this->time_step) + (velocityMult * BulletVel.y)
         //BulletVel.z * velocityMult                                            //Z for when you transition to the game's 3D coords
     };
 
-    BulletVel = dragged;
+    BulletVel = dragged_vel;
 
     vec2 new_pos{
-        (dragged.x * this->time_step) + BulletPos.x,
-        (dragged.y * this->time_step) + BulletPos.y
+        (dragged_vel.x * this->time_step) + BulletPos.x,
+        (dragged_vel.y * this->time_step) + BulletPos.y
         //(dragged.z * this->time_step) + BulletPos.z                           //Z for when you transition to the game's 3D coords
     };
 
