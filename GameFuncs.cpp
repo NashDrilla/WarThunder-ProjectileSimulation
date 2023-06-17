e9db15fe98c5bec872c2f86769deffd2cbb787cf
Faraday <57951540+NashDrilla@users.noreply.github.com>
2023-06-17 19:24:08 +0100

    Update GameFuncs.cpp

--- GameFuncs.cpp
+++ GameFuncs.cpp
@@ -2,52 +2,50 @@
 
 float GameFuncs::GetDragConstant(float LocalPosY)
 {
-    const auto max_alt = 18300.0f;
-    const auto alt_mult = 1.225f;
-    const auto clamped_alt = fmin(LocalPosY, max_alt);
-    const auto unk_1 = 2.2871901e-19f, unk_2 = 5.8355603e-14f,
-        unk_3 = 0.00000000353118f, unk_4 = 0.000095938703f;
-
-
-    return alt_mult *
-        ((max_alt / std::fmax(LocalPosY, max_alt)) *
-            ((((((unk_1 * clamped_alt) - unk_2) * clamped_alt) + unk_3) *
-                clamped_alt) -
-                unk_4) *
-            clamped_alt +
-            1.0f);
+	const auto maxAlt = 18300.0f;
+	const auto altMult = 1.225f;
+	const auto clampedAlt = fmin(LocalPosY, maxAlt);
+	const auto unk1 = 2.2871901e-19f, unk2 = 5.8355603e-14f,
+		unk3 = 0.00000000353118f, unk4 = 0.000095938703f;
+	return altMult *
+		((maxAlt / std::fmax(LocalPosY, maxAlt)) *
+			((((((unk1 * clampedAlt) - unk2) * clampedAlt) + unk3) *
+				clampedAlt) -
+				unk4) *
+			clampedAlt +
+			1.0f);
 }
 
-float GameFuncs::GetBallisticCoeff(float BulletLength, float BulletMass, float BulletCaliber, float DragConstant)
+loat GameFuncs::GetBallisticCoeff(float BulletLength, float BulletMass, float BulletCaliber, float DragConstant)
 {
-    return -1.0f *
-        (DragConstant * static_cast<float>(M_PI) * 0.5f *
-            std::pow(BulletCaliber * 0.5f, 2.0f) *
-            BulletLength) /
-        BulletMass;
+	return -1.0f *
+		(DragConstant * static_cast<float>(M_PI) * 0.5f *
+			std::pow(BulletCaliber * 0.5f, 2.0f) *
+			BulletLength) /
+		BulletMass;
 }
 
 void GameFuncs::ApplyDrag(float BallisticCoeff, vec2& BulletVel, vec2& BulletPos)
 {
-    float delta_speed = (BallisticCoeff * this->time_step) * BulletVel.Length();
-    float velocityMult = 1.0f;
-    if (delta_speed < 1.0f || delta_speed > 1.0f) {
-        velocityMult = (delta_speed / (1.0f - delta_speed)) + 1.0f;
-    }
+	float deltaSpeed = (BallisticCoeff * this->timeStep) * BulletVel.Length();
+	float velocityMult = 1.0f;
+	if (deltaSpeed < 1.0f || deltaSpeed > 1.0f) {
+		velocityMult = (deltaSpeed / (1.0f - deltaSpeed)) + 1.0f;
+	}
 
-    vec2 dragged_vel{
-        BulletVel.x * velocityMult,
-        (this->gravity * this->time_step) + (velocityMult * BulletVel.y)
-        //BulletVel.z * velocityMult                                            //Z for when you transition to the game's 3D coords
-    };
+	vec2 draggedVel{
+	BulletVel.x * velocityMult,
+	(this->gravity * this->timeStep) + (velocityMult * BulletVel.y)
+	//BulletVel.z * velocityMult                                            //Z for when you transition to the game's 3D coords
+	};
 
-    BulletVel = dragged_vel;
+	BulletVel = draggedVel;
 
-    vec2 new_pos{
-        (dragged_vel.x * this->time_step) + BulletPos.x,
-        (dragged_vel.y * this->time_step) + BulletPos.y
-        //(dragged.z * this->time_step) + BulletPos.z                           //Z for when you transition to the game's 3D coords
-    };
+	vec2 newPos{
+		(draggedVel.x * this->timeStep) + BulletPos.x,
+		(draggedVel.y * this->timeStep) + BulletPos.y
+		//(dragged.z * this->timeStep) + BulletPos.z                           //Z for when you transition to the game's 3D coords
+	};
 
-    BulletPos = new_pos;
+	BulletPos = newPos;
 }
