b658d0339fdcdb84e410b978d949d701514243a7
Faraday <57951540+NashDrilla@users.noreply.github.com>
2023-06-17 19:25:59 +0100

    Update Main.cpp

--- Main.cpp
+++ Main.cpp
@@ -8,26 +8,25 @@
 int main()
 {
 	GameFuncs game;
+    // Example of War thunders bullet coefficient
+    float bulletCoeff = -0.0005f;
+    float gravity = -9.81f;
+    // Degrees. Needed to create a velocity.
+    float launchAngle = 45.f;
+    float bulletSpeed = 30.f; // m/s
 
-	//Example of War thunders bullet coefficient
-	float bc = -0.0005f;
-	float gravity = -9.81f;
-	//Degrees. Needed to create a velocity.
-	float launch_angle = 45.f;
-	float bullet_speed = 30.f;//m/s
+    vec2 bulletPosition = { 0,0 };
+    vec2 bulletFireDirection = { cos(DEG2RAD(launchAngle)), sin(DEG2RAD(launchAngle)) };
+    vec2 bulletVelocity = bulletFireDirection * bulletSpeed;
 
-	vec2 bullet_position = { 0,0 };
-	vec2 bullet_fire_direction = { cos(DEG2RAD(launch_angle)), sin(DEG2RAD(launch_angle)) };
-	vec2 bullet_velocity = bullet_fire_direction * bullet_speed;
+    for (float t = 0; t < 5.f; t += game.timeStep) {
+        if (bulletPosition.y < -0.f)
+            break;
 
-	for (float t = 0; t < 5.f; t += game.time_step) {
-		if (bullet_position.y < -0.f)
-			break;
+        printf("Time: %f\nPosition [X:%2.2f || Y:%2.2f]\n\n", t, bulletPosition.x, bulletPosition.y);
 
-		printf("Time: %f\nPosition [X:%2.2f || Y:%2.2f]\n\n", t, bullet_position.x, bullet_position.y);
+        game.ApplyDrag(bulletCoeff, bulletVelocity, bulletPosition);
+    }
 
-		game.ApplyDrag(bc, bullet_velocity, bullet_position);
-	}
-
-	printf("Finished\n");
+    printf("Finished\n");
 }
